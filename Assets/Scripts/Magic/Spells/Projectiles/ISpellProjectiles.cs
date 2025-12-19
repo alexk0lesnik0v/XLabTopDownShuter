c4175f430e72e0c52bf4e5508ea1bb4c3408a0fe
alexk0lesnik0v <alexander74k@mail.ru>
2025-12-19 10:33:07 +0300

    Add projictiles

---
+++ Assets/Scripts/Magic/Spells/Projectiles/ISpellProjectiles.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using Magic.Effects;
+using System.Collections.Generic;
+
+namespace Magic.Spells.Projectiles
+{
+    public interface ISpellProjectiles
+    {
+        public void Initialize(Vector3 targetPosition, float speed, IReadOnlyList<IEffect> effects);
+    }
+}
