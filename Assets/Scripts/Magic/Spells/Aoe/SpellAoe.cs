fa60519b4fb0e9108fa9bad0bd5e247722b2780a
alexk0lesnik0v <alexander74k@mail.ru>
2025-12-19 10:41:11 +0300

    Add spellAoe

---
+++ Assets/Scripts/Magic/Spells/Aoe/SpellAoe.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections.Generic;
+using Magic.Effects;
+using UnityEngine;
+
+namespace Magic.Spells.Aoe
+{
+    public sealed class SpellAoe : MonoBehaviour, ISpellAoe
+    {
+        public void Initialize(Vector3 targetPosition, float radius, IReadOnlyCollection<IEffect> effects)
+        {
+            var colliders = Physics.OverlapSphere(targetPosition, radius);
+
+            foreach (var collider in colliders)
+            {
+                if (collider.TryGetComponent<IEffectable>(out var effectable))
+                {
+                    foreach (var effect in effects)
+                    {
+                        effect.Apply(effectable);
+                    }
+                }
+            }
+        }
+    }
+}
