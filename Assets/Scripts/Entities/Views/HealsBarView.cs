970adedf058f616cb26fea0dec8ab06cd28c46f5
Alexander Kolesnikov <alexander74k@mail.ru>
2026-01-16 18:08:28 +0300

    finish lesson 21

---
+++ Assets/Scripts/Entities/Views/HealsBarView.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+namespace Entities.Views
+{
+    public class HealsBarView : MonoBehaviour
+    {
+        [SerializeField] private Image m_bar;
+        [SerializeField] private HealthComponent m_healthComponent;
+
+        private void OnEnable()
+        {
+            m_healthComponent.ValueChanged += SetValue;
+        }
+
+        private void OnDisable()
+        {
+            m_healthComponent.ValueChanged -= SetValue;
+        }
+
+        private void SetValue()
+        {
+            m_bar.fillAmount = (float)m_healthComponent.value;
+        }
+    }
+}
