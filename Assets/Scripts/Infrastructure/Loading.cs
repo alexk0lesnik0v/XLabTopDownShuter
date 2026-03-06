b0069a1e2e13c13a30ba2d19c77f458e1fa83d45
Alexander Kolesnikov <alexander74k@mail.ru>
2026-03-06 18:02:26 +0300

    Lesson finished

---
+++ Assets/Scripts/Infrastructure/Loading.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+namespace Infrastructure
+{
+    public class Loading : MonoBehaviour
+    {
+        [SerializeField] private Image m_loading;
+        
+        private string m_nameScene;
+        
+        public void LoadScene(string nameScene)
+        {
+            gameObject.SetActive(true);
+            StartCoroutine(LoadSceneAsync(nameScene));
+        }
+
+        private IEnumerator LoadSceneAsync(string nameScene)
+        {
+            m_loading.fillAmount = 0;
+            
+            AsyncOperation operation = SceneManager.LoadSceneAsync(nameScene);
+            yield return operation;
+            
+            m_loading.fillAmount = 0.5f;
+
+            const int steps = 10;
+            var delta = 1 - m_loading.fillAmount;
+
+            for (var i = 0; i < steps; i++)
+            {
+                yield return new WaitForSeconds(0.5f);
+                m_loading.fillAmount += delta / steps;
+            }
+            
+            gameObject.SetActive(false);
+        }
+    }
+}
---
+++ Assets/Scripts/UI/PauseMenuView.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace UI
+{
+    public class PauseMenuView : MonoBehaviour
+    {
+        public event Action ContinueClick;
+        public event Action MainMenuClick;
+        
+        [SerializeField] private Button m_continue;
+        [SerializeField] private Button m_mainMenu;
+
+        private void OnEnable()
+        {
+            m_continue.onClick.AddListener(OnContinueClick);
+            m_continue.onClick.AddListener(OnMainMenuClick);
+        }
+        
+        private void OnDisable()
+        {
+            m_continue.onClick.RemoveListener(OnContinueClick);
+            m_continue.onClick.RemoveListener(OnMainMenuClick);
+        }
+
+        private void OnMainMenuClick() => 
+            MainMenuClick?.Invoke();
+
+        private void OnContinueClick() => 
+            ContinueClick?.Invoke();
+    }
+}
