437b54ceca3d7ed871dd4db461787403e54aa0b1
Alexander Kolesnikov <alexander74k@mail.ru>
2026-03-10 18:01:49 +0300

    finish lesson 34

---
+++ Assets/Scripts/Infrastructure/BootstrapA.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using UnityEngine;
+
+namespace Infrastructure
+{
+    [DefaultExecutionOrder(-500)]
+    public class BootstrapA :MonoBehaviour
+    {
+        private void Awake()
+        {
+            
+        }
+    }
+}
---
+++ Assets/Scripts/Infrastructure/States/GameplayEntryState.cs
@@ -0,0 +1,15 @@
+﻿namespace Infrastructure.States
+{
+    public class GameplayEntryState : IState
+    {
+        public void Enter()
+        {
+            
+        }
+
+        public void Exit()
+        {
+            
+        }
+    }
+}
